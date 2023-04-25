4c9ac42c4b05b3933314f899778fa764c43654b2
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-04-25 16:53:09 -0300

    changes in login

--- src/pages/login/Login.js
+++ src/pages/login/Login.js
@@ -1,16 +1,39 @@
 import { Button, Form } from 'react-bootstrap'
-import React from 'react'
+import React, { useState } from 'react'
 import "./login.css"
+const EMAIL_REGEX = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;
+const PWD_REGEX = /.{6,16}$/;
 
 
 export const Login = () => {
+  const [email, setEmail] = useState("")
+  const [emailFocus, setEmailFocus] = useState(false)
+  const [validEmail, setValidEmail] = useState("")
+  
+  const [pwd, setPwd] = useState("")
+  
+  
+
+
+
   return (
     <div className='login_container'>
         <Form className="form_container" >
           <h1>Login</h1>
           <Form.Group className="mb-3" controlId="formBasicEmail">
             <Form.Label>Email address</Form.Label>
-            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Control 
+              type="email" 
+              placeholder="user@mail.com"
+              autoComplete='off'
+              onChange={(e)=>{setEmail(e.target.value)}}
+              value={email}
+              maxLength="24"
+              aria-invalid={validEmail ? 'false' : 'true'}
+              aria-describedby="emailnote"
+              onFocus={() => setEmailFocus(true)}
+              onBlur={() => setEmailFocus(false)}
+            />
           </Form.Group>
           <Form.Group className="mb-3" controlId="formBasicPassword">
             <Form.Label>Password</Form.Label>
