0a136d8ae30a03ab476d366f6feb6c331f6a0b02
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-05-03 14:41:19 -0300

    ident in details modal

--- src/components/clientsDetails/ModalDetails.js
+++ src/components/clientsDetails/ModalDetails.js
@@ -4,24 +4,24 @@ import { Button, Modal } from 'react-bootstrap'
 const ModalDetails = ({show, setShow, client}) => {
   return (
     <Modal show={show} onHide={() => setShow(false)}>
-    <Modal.Header closeButton>
-      <Modal.Title>{client.barName}</Modal.Title>
-    </Modal.Header>
-    <Modal.Body>
-        <ul>
-            <li>Manager: {client.barManager}</li>
-            <li>Location: {client.location}</li>
-            <li>email:{client.email}</li>
-        </ul>
-    </Modal.Body>
-    <Modal.Footer>
-      <Button variant="secondary" onClick={() => setShow(false)}>
-        Close
-      </Button>
-      <Button variant="primary" onClick={() => setShow(false)}>
-        Save Changes
-      </Button>
-    </Modal.Footer>
+        <Modal.Header closeButton>
+            <Modal.Title>{client.barName}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <ul>
+                <li>Manager: {client.barManager}</li>
+                <li>Location: {client.location}</li>
+                <li>email:{client.email}</li>
+            </ul>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button variant="secondary" onClick={() => setShow(false)}>
+                Close
+            </Button>
+            <Button variant="primary" onClick={() => setShow(false)}>
+                Save Changes
+            </Button>
+        </Modal.Footer>
   </Modal>
   )
 }
