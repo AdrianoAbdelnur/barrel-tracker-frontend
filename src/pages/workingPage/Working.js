1122be074f0dc0cd771303f9b0602a9af206ceca
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-08-23 15:37:59 -0300

    Working Image added

--- src/pages/workingPage/Working.js
+++ src/pages/workingPage/Working.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import './workingPage.css'
+import sleeping from './../../assets/img/sleeping.jpg'
+import { Image } from 'react-bootstrap'
 
 const Working = () => {
   return (
@@ -7,6 +9,7 @@ const Working = () => {
         <div className='information_container'>
             <h1>Page under construction</h1>
             <h3>We are working in this page</h3>
+            <Image src={sleeping} className='workingImage' rounded />
         </div>
     </div>
   )
