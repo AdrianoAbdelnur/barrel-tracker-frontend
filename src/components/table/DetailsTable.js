6e2b43dcfed78115e5262b1ae82024c14d7506cd
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-09-13 10:02:08 -0300

    Product stock tablle added

---
+++ src/components/table/DetailsTable.js
@@ -0,0 +1,44 @@
+import React, { useEffect } from 'react'
+import { Table } from 'react-bootstrap'
+
+const DetailsTable = ({styles, barrels}) => {
+    useEffect(() => {
+        console.log(styles)
+    }, [styles])
+    
+    return (
+        <Table striped bordered hover>
+            <thead>
+            <tr>
+                    <th>#</th>
+                    <th>Style</th>
+                    <th className='text-center'>50 liters</th>
+                    <th className='text-center'>30 liters</th>
+                    <th className='text-center'>20 liters</th>
+                    <th className='text-center'>10 liters</th>
+                    <th className='text-center'>5 liters</th>
+                    </tr>
+            </thead>
+            <tbody>
+                {styles?.map((style, index )=> {
+                    return(
+
+                        <tr>
+                            <td className='text-center'>{index}</td>
+                            <td>{style.name}</td>
+                            <td className='text-center'>{barrels.filter(barrel=> barrel.statusBarrel === 'full in factory' && barrel.capacity === 50 && barrel.style._id === style._id).length }</td>
+                            <td className='text-center'>{barrels.filter(barrel=> barrel.statusBarrel === 'full in factory' && barrel.capacity === 30 && barrel.style._id === style._id).length }</td>
+                            <td className='text-center'>{barrels.filter(barrel=> barrel.statusBarrel === 'full in factory' && barrel.capacity === 20 && barrel.style._id === style._id).length }</td>
+                            <td className='text-center'>{barrels.filter(barrel=> barrel.statusBarrel === 'full in factory' && barrel.capacity === 10 && barrel.style._id === style._id).length }</td>
+                            <td className='text-center'>{barrels.filter(barrel=> barrel.statusBarrel === 'full in factory' && barrel.capacity === 5 && barrel.style._id === style._id).length }</td>
+                            
+                        </tr>
+                    )
+                })}
+                
+            </tbody>
+        </Table>
+    )
+}
+
+export default DetailsTable
