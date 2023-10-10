53ed49a32fc718837d617c9e851cb08066fef210
Adriano Abdelnur <adrianoabdelnur@hotmail.com>
2023-10-10 10:00:38 -0300

    Correction in addCost

--- src/pages/costs/addCost/AddCost.js
+++ src/pages/costs/addCost/AddCost.js
@@ -70,9 +70,6 @@ const AddCost = () => {
                     name='cost'
                 />
             </Form.Group>
-            {/* <Form.Group className="mb-3" controlId="formBasicCheckbox">
-                <Form.Check type="checkbox" label="Check me out" />
-            </Form.Group> */}
             <Button variant="primary" type="submit">
                 Submit
             </Button>
